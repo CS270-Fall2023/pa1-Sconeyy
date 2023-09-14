a929616abfb7ca63ea30fb43dfc1b4310bfda936
sconeyy <jcgauvin@nic.edu>
2023-09-14 13:45:50 -0700

    Comments and var names

--- token.c
+++ token.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-int getTokens(char *s, char ***args)
+int getTokens(char *string, char ***arguments)
 {
-    int stringLength = strlen(s);
+    int stringLength = strlen(string);
 
     if (stringLength == 0) // Ends program if string has no length or if inputString breaks for some reason
     {
         return -1;
     }
 
-    *args = malloc(sizeof(char **)); // Allocate a pointer-sized memory piece for token storage
-    if (*args == NULL)
+    *arguments = malloc(sizeof(char **)); // Allocate a pointer-sized memory piece for token storage
+    if (*arguments == NULL)
     {
         return -1;
     }
@@ -21,21 +21,21 @@ int getTokens(char *s, char ***args)
     int startOfToken = 0; // Start of each counted token
     int endOfToken = 0;   // End of each counted token. The start value will be subtracted from the end value to determine each token length
 
-    char **tokenStorage = *args;
+    char **tokenStorage = *arguments;
 
     while (startOfToken < stringLength)
     {
-        while (s[endOfToken] != ' ' && endOfToken < stringLength)
+        while (string[endOfToken] != ' ' && endOfToken < stringLength) // stringLength is the length of the inputString
         {
             endOfToken++;
-            // If the current character isn't a space, increment the token ending point
+            // While the end token position is NOT a space AND it is less than the calculated string length, increment the end token position
         }
 
         int tokenLength = endOfToken - startOfToken; // Token length calculation
 
         char *token = malloc(sizeof(char) * (tokenLength + 1)); // Allocating memory for the counted token AND the null terminator at the end
 
-        memcpy(token, s + startOfToken, tokenLength); // memcpy function given by Ol' Walt
+        memcpy(token, string + startOfToken, tokenLength); // memcpy function given by Ol' Walt
 
         token[tokenLength] = '\0'; // Add the null terminator onto the counted token
 
@@ -43,7 +43,7 @@ int getTokens(char *s, char ***args)
         tokenCount++;
 
         tokenStorage = realloc(tokenStorage, sizeof(char **) * (tokenCount + 1));
-        *args = tokenStorage;
+        *arguments = tokenStorage;
 
         startOfToken = endOfToken + 1; // Set the token starting point to the start of the next token
         endOfToken = startOfToken;     // Set the token ending point to the starting point to repeat the process
